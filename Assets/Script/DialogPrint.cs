d6fda21346eea97b6792152b6f46ea4e57c37337
HeoTaein <huteain@naver.com>
2016-08-05 00:17:04 +0900

    유니티파일 커밋

---
+++ Assets/Script/DialogPrint.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DialogPrint : MonoBehaviour
+{
+    //DialogXMLData DXD = new DialogXMLData();
+    GameManager GM = new GameManager();
+    Text tempText;
+    DialogXMLData DXD;
+
+    //-------------테스트용-------------------
+    //public List<string> dialog = new List<string>();
+    public string[] dialog;
+    
+    public bool init = true;
+    bool play = true;
+    
+    //-------------테스트용-------------------
+    
+
+
+    void Awake()
+    {
+        tempText = this.GetComponent<Text>();
+        DXD = this.GetComponentInParent<DialogXMLData>();
+    }
+
+    void Update()
+    {
+        if (init)
+        {
+            Debug.Log(DXD.text.Count);
+            //초기화를 제어 -> GM에서 제어할 예정
+            init = false;
+            dialog = DXD.textArr;
+            
+
+        }
+        if(play)
+        {
+            //플레이를 제어 -> Gm에서 제어할 예정
+            play = false;
+            //코루틴을 이용해서 다이어로그 프린트함
+            StartCoroutine(Dialog(1.0f));
+        }
+    }
+    // 다이얼로그를 1초마다 넘김
+    IEnumerator Dialog(float time)
+    {
+        for (int i = 0; i < dialog.Length; i++)
+        {
+            tempText.text = dialog[i];
+            yield return new WaitForSeconds(time);
+        }
+    }
+
+
+}
+
---
+++ Assets/Script/DialogXMLData.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+public class DialogXMLData : MonoBehaviour
+{
+
+    public string _FileName = "DialogDataXML";
+    //public TextAsset _ExpXmlFile;
+    public List<string> charName = new List<string>();
+    public List<string> text = new List<string>();
+
+    public string[] textArr;
+
+    void Awake()
+    {
+        /*
+        GM에서 불러와야 하는 데이터만 가져와서 프린트 해야함으로
+        GM에 신 넘버가 필요함   
+        */
+        
+        Xml_Load(_FileName);
+    }
+
+    void Xml_Load(string Filename)
+    {
+        //xml생성
+        TextAsset textAsset = (TextAsset)Resources.Load("XML/" + Filename);
+        XmlDocument xmldoc = new XmlDocument();
+        xmldoc.LoadXml(textAsset.text);
+
+        // 전체 가져오기
+        XmlNodeList nodes = xmldoc.SelectNodes("DialogSet/Line");
+
+        // 데이터 가져오기! 함수
+        foreach (XmlNode node in nodes)
+        {
+            //Debug.Log(Convert.ToInt16(node.Attributes.SetNamedItem("Levelnum").Value));
+            charName.Add(node.SelectSingleNode("CharName").InnerText);
+            text.Add(node.SelectSingleNode("Text").InnerText);
+            //.InnerText
+        }
+        textArr = text.ToArray();
+    }
+}
