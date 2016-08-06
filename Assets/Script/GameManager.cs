57c437e3ee449407027a659113693fd34d9722ce
HeoTaein <huteain@naver.com>
2016-08-07 04:15:03 +0900

    이미지표현됨

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,15 +8,19 @@ public class GameManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+
+        //시나리오를 불렀을 경우!
 	    if(sinarioLoadChk)
         {
             sinarioLoadChk = false;
             Transform dialogparents = GameObject.Find("Canvas").GetComponent<Transform>();
             GameObject dialogboxprefeb = Resources.Load<GameObject>("prefebs/dialogbox");
-            GameObject dialogUI = Instantiate(dialogboxprefeb, dialogboxprefeb.transform.position, Quaternion.identity) as GameObject;
-            dialogUI.name = "dialogUI";
-            dialogUI.transform.parent = dialogparents;
-            //dialogUI.
+            GameObject dialogUI = Instantiate(dialogboxprefeb) as GameObject;
+            //GameObject dialogUI = (GameObject)GameObject.Instantiate(dialogboxprefeb);
+            dialogUI.name = "dialogbox";
+            
+            //할당된 부모의 정보를 가져가지 않도록 하는 구문
+            dialogUI.transform.SetParent(dialogparents,false);
         }
 	}
 }
